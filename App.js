c7e30a006a88ede892c121dc705c1f14d72610bc
Ariane Evans <ariane.evans@student.makeschool.com>
2020-06-23 16:10:49 -0700

    [add] two views with buttons

--- App.js
+++ App.js
@@ -23,6 +23,12 @@ function DetailScreen({ navigation }) {
         title="Go to Details... again"
         onPress={() => navigation.push('Details')}
       />
+      <Button 
+        title="Go to Home" onPress={() => navigation.navigate('Home')}
+      />
+      <Button
+        title="Go back" onPress={() => navigation.goBack()}
+      />
     </View>
   )
 }
